12eddb904c297a0859b4c19ccfda14b5c7f9110a
Mariia Romaniuk <m.grabelnikova@gmail.com>
2020-05-28 20:20:58 -0400

    Completed with all extra credits

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,17 +1,32 @@
 'use strict';
 
-// Complete this algo
-const binarySearch = (array, target) => {
-	
-};
-
 /*
-	EXTRA CREDIT:
-
-	Can you augment the function above to run in constant O(1) space?
-	This means, you cannot edit/copy the original array!
-	How can we use other pieces of data, like pointers, to avoid slicing?
-
+	Function runs in constant O(1) space - we're not edit/copy the original array!
+	We use other piece of data, like pointers, to avoid slicing
 */
+const binarySearch = (array, target) => {
+	if (array.length === 0)
+	   return false;
+	else if (array.length === 1)
+	   if (array[0] === (target))
+		  return true;
+
+	let start = 0;
+	let end = array.length-1;
+
+	while (start <= end) {
+		let midPoint = Math.floor((start + end) / 2);
+		if (array[midPoint] === target) {
+			return true;
+		} else {
+			if (array[midPoint] < target) {
+				start = midPoint + 1;
+			} else {
+				end = midPoint - 1;
+			}
+		}
+	}
+	return false;
+};
 
 module.exports = binarySearch
