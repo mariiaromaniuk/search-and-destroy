84491be0efa2980f9dbb7df8d5bd283709fc51ef
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-05-28 21:57:04 -0400

    Update min-jumps.js

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -5,8 +5,7 @@ const minJumps = ar => {
     let start = 0,
         jumps = 0,
         len = ar.length-1,
-        maxIdx,
-        max = 0;
+        maxIdx, max;
 
     if (ar.length <= 2 || start+ar[start] >= len)
         return ++jumps;
@@ -27,18 +26,5 @@ const minJumps = ar => {
     }
     return jumps;
 };
-
-
-const test1 = [1, 1];
-const test2 = [6, 3, 2, 1];
-const test3 = [2, 8, 4, 3, 2, 9, 6, 8];
-const test4 = [4, 4, 2, 7, 1, 1, 1, 1, 3, 7, 2];
-const test5 = [2, 4, 1, 1, 2, 3, 7, 1, 1, 3];
-
-console.log(minJumps(test1));
-console.log(minJumps(test2));
-console.log(minJumps(test3));
-console.log(minJumps(test4));
-console.log(minJumps(test5));
 
 module.exports = minJumps
