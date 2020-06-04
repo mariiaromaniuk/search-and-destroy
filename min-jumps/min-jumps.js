fa771486d397f0c3f77018ab6bb33f775385926f
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2020-06-04 14:22:17 -0400

    Update min-jumps.js

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -28,3 +28,17 @@ const minJumps = ar => {
 };
 
 module.exports = minJumps
+
+
+// const minJumps = arr => {
+//   let jumps = arr.map(el => Infinity)
+//   jumps[0] = 0;
+
+//   for (let i = 1; i < jumps.length; i++) {
+//     for (let j = 0; j <= i; j++) {
+//       if (j + arr[j] >= i) 
+//         jumps[i] = Math.min(jumps[i], jumps[j] + 1)
+//     }
+//   }
+//   return jumps[arr.length - 1];
+// };
